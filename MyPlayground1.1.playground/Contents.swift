aff49cd6aa18c22414602ab32c9dd080adf14a61
Qi Zhao <zhaoqistevens@outlook.com>
2022-11-11 15:14:32 +0800

    add the latest version of swift tour 1.1

---
+++ MyPlayground1.1.playground/Contents.swift
@@ -0,0 +1,49 @@
+//import UIKit
+//
+//var greeting = "Hello, playground"
+//
+//String ayaya = "AYAYAYAYYAAY"
+
+import Foundation
+var myName: String = "Qi Zhao"
+var age = 28
+var myCandy = "doesNotExist"
+let NOTANUMBER = -1
+myName = "c"
+print("\(myName + myName) \(age)")
+print("hello this is not my name: \(myName)")
+print("my favorite candy is \(myCandy)")
+
+let minuteNow = 12
+// this is a simple commment
+// this is a complex one ///////////////////
+////////////////
+///
+///ok ok
+///i got it
+///
+myName
+age
+NOTANUMBER
+print(NOTANUMBER)
+7/4
+7/4.0
+7.0/4.0
+7.0/4
+var ans = 8/4
+var someDouble: Double = 12345.1234567890
+var someString = String(ans)
+var someFloat: Float = 12345.1234567890
+8.0/4
+8.0/4.0
+let feet = 5
+let inches = 7
+let height = feet*12+inches
+var i = 0
+
+while i < 10 {
+    ans += i
+    i += 1
+}
+print("end of loop")
+
